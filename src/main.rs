31a04070837c89e2ddf11c0d04a7551dfca430ef
Mariluskithings (Mario Rosell) <hyper1516gamer@gmail.com>
2024-09-19 22:58:32 +0200

    xd

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
-use reqwest;
-use tokio;
-use web_view::{self, Content, WebViewBuilder};
-use select::document::Document;
-use select::predicate::Name;
-
 #[tokio::main]
 async fn main() {
-    let res: reqwest::Response = reqwest::get("https://google.com")
+    let url = "https://google.com";
+    let res = reqwest::get(url)
         .await
         .expect("Failed to send request");
-    let body: String = res.text()
-        .await
-        .expect("Failed to read response text");
-    let raw_html: String = body.trim().to_string();
-    println!("{}", raw_html);
 
-    let document = Document::from_read(raw_html.as_bytes()).unwrap();
-    let title = "huh";
-    let title_string = format!("ReSearch - {}", title);
-    let web_view = WebViewBuilder::new()
-        .title(&title_string) // Set the window title
-        .content(Content::Html(&raw_html)) // Set the initial HTML content
+    let final_url = res.url().clone();
+    let web_view = web_view::builder()
+        .title("ReSearch") // Set the window title
         .size(800, 600) // Set the window size
         .resizable(true) // Allow the window to be resized
         .user_data(()) // Specify user data
         .invoke_handler(|_webview, _arg| Ok(())); // Specify an invoke handler
 
-web_view.run().unwrap(); // Run the event loop
+    let web_view = web_view.build().unwrap();
+    web_view.load_html("<html><body><iframe src=\"" + &final_url.to_string() + "\" width=\"100%\" height=\"100%\"></iframe></body></html>").unwrap();
+    web_view.run().unwrap(); // Run the event loop
 }
