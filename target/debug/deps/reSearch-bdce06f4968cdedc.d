652ef9b3677636ec7db384416b6e1afed62756e9
Mariluskithings (Mario Rosell) <hyper1516gamer@gmail.com>
2024-09-21 11:32:28 +0200

    idk

---
+++ target/debug/deps/reSearch-bdce06f4968cdedc.d
@@ -0,0 +1,5 @@
+/home/mario/reSearch/target/debug/deps/libreSearch-bdce06f4968cdedc.rmeta: src/main.rs
+
+/home/mario/reSearch/target/debug/deps/reSearch-bdce06f4968cdedc.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/reSearch-c7ff7a5b67ab3e55.d
@@ -0,0 +1,5 @@
+/home/mario/reSearch/target/debug/deps/libreSearch-c7ff7a5b67ab3e55.rmeta: src/main.rs
+
+/home/mario/reSearch/target/debug/deps/reSearch-c7ff7a5b67ab3e55.d: src/main.rs
+
+src/main.rs:
